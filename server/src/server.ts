a724cd30751d71553a9922a6184d754ca7606227
Niklas Trapp <niklas031712@gmail.com>
2025-01-31 23:26:13 +0100

    update for deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,12 +5,12 @@ dotenv.config();
 import app from './app';
 import connectDB from './config/database';
 
-const PORT = process.env.PORT || 8000;
+const PORT = parseInt(process.env.PORT as string) || 8000;
 
 // Connect to Database
 connectDB();
 
 // Start server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
