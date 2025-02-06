0bcab8bcc63d5475c38e9045d1986a2056e6abf8
Niklas Trapp <niklas031712@gmail.com>
2025-02-06 16:42:12 +0100

    api | cleanup

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,28 +1,25 @@
-// src/app.ts
 import express from 'express';
-import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import authRoutes from './routes/authRoutes';
 import scrambleRoutes from './routes/scrambleRoutes';
+import solveRoutes from './routes/solveRoutes';
 
 const app = express();
 
-// Middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-  credentials: true  // Important for cookies
+  credentials: true
 }));
 
 app.set('trust proxy', 1);
 
-// Routes
 app.use('/api/auth', authRoutes);
 app.use('/api/cube', scrambleRoutes)
+app.use('/api/solve', solveRoutes)
 
-// Error handling middleware
 app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
   console.error(err.stack);
   res.status(500).json({ message: 'Something went wrong!' });
