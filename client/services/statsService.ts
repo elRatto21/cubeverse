2c668f55c905a1ccd4384da0a6806937462e693b
Niklas Trapp <niklas031712@gmail.com>
2025-02-07 11:50:03 +0100

    ui: add new service for stats

---
+++ client/services/statsService.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export const getDashboardStats = async () => {
+  try {
+    const response = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/api/solve/alltime`,
+      {
+        withCredentials: true,
+      }
+    );
+
+    return response.data.data;
+  } catch (error) {
+    console.log(error);
+    return {};
+  }
+};
