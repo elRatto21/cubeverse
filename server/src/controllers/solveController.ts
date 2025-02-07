d8c099a7f46d599e42431731c55ae94013e03dc3
Niklas Trapp <niklas031712@gmail.com>
2025-02-07 11:49:40 +0100

    api | improve solve functions

--- server/src/controllers/solveController.ts
+++ server/src/controllers/solveController.ts
@@ -36,38 +36,72 @@ export const solveController = {
 
   getSolves: async (req: Request, res: Response) => {
     try {
+      console.log(req.query);
 
-        console.log(req.query)
+      let limit = parseInt(req.query.limit as string);
 
-        let limit = parseInt(req.query.limit as string)
+      const event = req.query.event;
 
-        const event = req.query.event
-
-        if (event === undefined) {
-            return res.status(500).json({ message: "Event is missing" })
-        }
-
-        if (limit !== undefined) {
-            limit = 30
-        }
-
-        const user_id = getUserFromCookie(req.headers.cookie ?? "")
-
-        let rawData: any[] = [];
-
-        switch(event) {
-            case "3x3":
-                rawData = await Solve.find({ event, user_id }).sort({ createdAt: -1 }).limit(limit)
-        }
-
-        const data = rawData.map(solve => solve.toObject())
-
-        return res.status(200).json({ data })
-    } catch (e) {
-        console.warn(e);
-        return res
-          .status(500)
-          .json({ message: "Something went wrong, try again." });
+      if (event === undefined) {
+        return res.status(500).json({ message: "Event is missing" });
       }
+
+      if (limit !== undefined) {
+        limit = 30;
+      }
+
+      const user_id = getUserFromCookie(req.headers.cookie ?? "");
+
+      let rawData: any[] = [];
+
+      if (event === "all") {
+        rawData = await Solve.find({ user_id })
+          .sort({ createdAt: -1 })
+          .limit(limit);
+      } else {
+        rawData = await Solve.find({ event, user_id })
+          .sort({ createdAt: -1 })
+          .limit(limit);
+      }
+
+      const data = rawData.map((solve) => solve.toObject());
+
+      return res.status(200).json({ data });
+    } catch (e) {
+      console.warn(e);
+      return res
+        .status(500)
+        .json({ message: "Something went wrong, try again." });
+    }
+  },
+
+  getAlltime: async (req: Request, res: Response) => {
+    try {
+      const user_id = getUserFromCookie(req.headers.cookie ?? "");
+
+      const pb = await Solve.findOne({ user_id, event: "3x3" })
+        .sort({ time: 1 })
+        .limit(1);
+      const worst = await Solve.findOne({ user_id, event: "3x3" })
+        .sort({ time: -1 })
+        .limit(1);
+      const avg = await Solve.aggregate([
+        { $match: { user_id } },
+        { $group: { _id: null, avgTime: { $avg: "$time" } } },
+      ]);
+
+      const data = {
+        pb,
+        worst,
+        avg,
+      };
+
+      return res.status(200).json({ data });
+    } catch (e) {
+      console.warn(e);
+      return res
+        .status(500)
+        .json({ message: "Something went wrong, try again." });
+    }
   },
 };
